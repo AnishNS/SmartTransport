c9637ee3101b6094b8cd0dc6290dad56a454047f
AnishNS <152582409+AnishNS@users.noreply.github.com>
2026-07-23 09:20:54 +0530

    Initialize Node.js backend server

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Smart Transport Backend Running"
+    });
+});
+
+
+module.exports = app;
